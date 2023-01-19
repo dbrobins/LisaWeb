78c24a310b3ae9634cb5a7427f68c14dcedc80e7
dbrobins <dbrobins@i4031.net>
2023-01-19 14:26:06 -0500

    Calculate and output GST, PST, and total.

--- shuttle/scripts/shuttle.js
+++ shuttle/scripts/shuttle.js
@@ -8,10 +8,16 @@ function displayValues() {
 
     // do some calculations
     let subtotal = amountPeople * 30;
+    let pstToPay = subtotal * 0.07;
+    let gstToPay = subtotal * 0.08;
+    let total = subtotal + gstToPay + pstToPay;
 
     // generate the output
     document.getElementById("subtotal").value = subtotal;
     output += `<p>You ordered a shuttle for ${amountPeople} ${amountPeople > 1 ? 'people' : 'person'}</p>`;
+    document.getElementById("pstToPay").value = pstToPay;
+    document.getElementById("gstToPay").value = gstToPay;
+    document.getElementById("total").value = total;
 
     document.getElementById("output").innerHTML = output;
     console.log(output);
